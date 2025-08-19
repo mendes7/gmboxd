580f4061f6fda7092a1b8d392074e56f2781bf77
Natan Mendes Ramos <natan@192.168.100.2>
2025-08-19 19:50:07 -0300

    feat: initialize project

---
+++ src/main/java/com/spring/gmboxd/GmboxdApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gmboxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmboxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmboxdApplication.class, args);
+	}
+
+}
