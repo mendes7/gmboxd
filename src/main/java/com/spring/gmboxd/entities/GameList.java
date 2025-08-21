53df6f2ba7bbfb87cd65886459a3a63990c6ceff
natan <natanmendesramos@gmail.com>
2025-08-21 12:20:54 -0300

    feat: add Belonging and GameList entities with composite key support

---
+++ src/main/java/com/spring/gmboxd/entities/GameList.java
@@ -0,0 +1,61 @@
+package com.spring.gmboxd.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    private UUID id;
+    private String name;
+
+    @PrePersist
+    public void prePersist() {
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+    }
+
+    public GameList() {
+    }
+
+    public GameList(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
