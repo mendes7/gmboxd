c1cbf02cdcddb1e7816638dd4771c4c69f65a1ba
natan <natanmendesramos@gmail.com>
2025-08-21 16:52:07 -0300

    feat: add GameDTO and implement findById method in GameService and GameController

--- src/main/java/com/spring/gmboxd/controllers/GameController.java
+++ src/main/java/com/spring/gmboxd/controllers/GameController.java
@@ -1,13 +1,16 @@
 package com.spring.gmboxd.controllers;
 
+import com.spring.gmboxd.dtos.GameDTO;
 import com.spring.gmboxd.dtos.GameMinDTO;
 import com.spring.gmboxd.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/games")
@@ -15,6 +18,11 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable UUID id) {
+        return gameService.findById(id);
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
--- src/main/java/com/spring/gmboxd/services/GameService.java
+++ src/main/java/com/spring/gmboxd/services/GameService.java
@@ -1,11 +1,17 @@
 package com.spring.gmboxd.services;
 
+import com.spring.gmboxd.dtos.GameDTO;
 import com.spring.gmboxd.dtos.GameMinDTO;
+import com.spring.gmboxd.entities.Game;
 import com.spring.gmboxd.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class GameService {
@@ -13,6 +19,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(UUID id) {
+        Game entity = gameRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game not found"));
+        return new GameDTO(entity);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll()
                 .stream()
